acfcfe6cc1708f7d3e310504cc7dd8f4e50675d7
Federico Gimenez <federico.gimenez@gmail.com>
2019-05-02 11:09:53 +0200

    inquirer menu

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const clear = require('clear');
 const figlet = require('figlet');
 const ProgressBar = require('progress');
 const https = require('https');
+const inquirer = require('./lib/inquirer');
 
 clear();
 console.log(
@@ -13,34 +14,10 @@ console.log(
   )
 );
 
-console.log(chalk.yellow('Downloading components...'));
+const run = async () => {
+  const answers = await inquirer.main();
+  console.log(`Selected: ${answers.action}`);
+  process.exit(0);
+};
 
-let req = https.request({
-  host: 'download.github.com',
-  port: 443,
-  path: '/visionmedia-node-jscoverage-0d4608a.zip'
-});
-
-req.on('response', (res) => {
-  let len = parseInt(res.headers['content-length'], 10);
-
-  console.log();
-  let bar = new ProgressBar('  downloading component1 [:bar] :rate/bps :percent :etas', {
-    complete: '=',
-    incomplete: ' ',
-    width: 20,
-    total: len
-  });
-
-  res.on('data', (chunk) => {
-    bar.tick(chunk.length);
-  });
-
-  res.on('end', () => {
-    console.log('\n');
-
-    console.log(chalk.green('Components downloaded'));
-  });
-});
-
-req.end();
+run();
