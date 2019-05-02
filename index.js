0738934049016ee7bfec431b3924a1ebbfa6ce77
Federico Gimenez <federico.gimenez@gmail.com>
2019-05-02 10:15:13 +0200

    download components mock

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const ProgressBar = require('progress');
+const https = require('https');
 
 clear();
 console.log(
@@ -10,3 +12,35 @@ console.log(
     figlet.textSync('Polkadot Deployer', { horizontalLayout: 'full' })
   )
 );
+
+console.log(chalk.yellow('Downloading components...'));
+
+let req = https.request({
+  host: 'download.github.com',
+  port: 443,
+  path: '/visionmedia-node-jscoverage-0d4608a.zip'
+});
+
+req.on('response', (res) => {
+  let len = parseInt(res.headers['content-length'], 10);
+
+  console.log();
+  let bar = new ProgressBar('  downloading component1 [:bar] :rate/bps :percent :etas', {
+    complete: '=',
+    incomplete: ' ',
+    width: 20,
+    total: len
+  });
+
+  res.on('data', (chunk) => {
+    bar.tick(chunk.length);
+  });
+
+  res.on('end', () => {
+    console.log('\n');
+
+    console.log(chalk.green('Components downloaded'));
+  });
+});
+
+req.end();
