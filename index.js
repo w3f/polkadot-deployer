1d12689051eba4cb99e7ef102c1cdb7229dfdbb1
Federico Gimenez <federico.gimenez@gmail.com>
2019-05-03 11:53:20 +0200

    init action stub

--- index.js
+++ index.js
@@ -3,9 +3,20 @@
 const process = require('process');
 const program = require('commander');
 const version = require('./lib/version');
+const init = require('./lib/actions/init');
 //const inquirer = require('./lib/inquirer');
 
-program.version(version.show())
+program
+  .version(version.show());
+
+program
+  .command('init')
+  .description('initialize the deployer tool')
+  .action((cmd) => {
+    init.do();
+  });
+
+program
   .parse(process.argv);
 
 /*
