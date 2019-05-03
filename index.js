5d66401ea0a54e54b6766f5018da06b8d44114fd
Federico Gimenez <federico.gimenez@gmail.com>
2019-05-03 12:01:03 +0200

    added list, create and destroy action stubs

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const process = require('process');
 const program = require('commander');
 const version = require('./lib/version');
 const init = require('./lib/actions/init');
+const list = require('./lib/actions/list');
+const create = require('./lib/actions/create');
+const destroy = require('./lib/actions/destroy');
 //const inquirer = require('./lib/inquirer');
 
 program
@@ -12,8 +15,29 @@ program
 program
   .command('init')
   .description('initialize the deployer tool')
-  .action((cmd) => {
+  .action(() => {
     init.do();
+  });
+
+program
+  .command('list')
+  .description('show the clusters already deployed')
+  .action(() => {
+    list.do();
+  });
+
+program
+  .command('create')
+  .description('deploys a new cluster')
+  .action(() => {
+    create.do();
+  });
+
+program
+  .command('destroy')
+  .description('deletes a deployed cluster')
+  .action(() => {
+    destroy.do();
   });
 
 program
