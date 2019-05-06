9629bce530664349f82998f22bb1171490f6ea1f
Federico Gimenez <federico.gimenez@gmail.com>
2019-05-06 10:25:14 +0200

    remove init command

--- index.js
+++ index.js
@@ -3,18 +3,12 @@
 const process = require('process');
 const program = require('commander');
 const version = require('./lib/version');
-const init = require('./lib/actions/init');
 const list = require('./lib/actions/list');
 const create = require('./lib/actions/create');
 const destroy = require('./lib/actions/destroy');
 
 program
   .version(version.show());
-
-program
-  .command('init')
-  .description('initialize the deployer tool')
-  .action(init.do);
 
 program
   .command('list')
