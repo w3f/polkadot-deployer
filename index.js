3c46bf787eed40628ea2861f31daaf7f09ef962b
Federico Gimenez <federico.gimenez@gmail.com>
2019-05-20 11:35:13 +0200

    verbose option

--- index.js
+++ index.js
@@ -23,16 +23,19 @@ program
   .command('create')
   .description('Deploys a new cluster.')
   .option('-c, --config [path]', 'path to config file')
+  .option('--verbose', 'Output extra info.')
   .action(create.do);
 
 program
   .command('destroy [name]')
   .description('Deletes a deployment.')
+  .option('--verbose', 'Output extra info.')
   .action(destroy.do);
 
 program
   .command('redeploy [name]')
   .description('Recreates a deployment on an existing cluster.')
+  .option('--verbose', 'Output extra info.')
   .action(redeploy.do);
 
 program
@@ -40,12 +43,15 @@ program
   .description('Creates deployments and runs benchmarks on them.')
   .option('-c, --config [filePath]', 'path to config file')
   .option('-o, --output [directoryPath]', 'path to output data directory')
+  .option('--verbose', 'Output extra info.')
   .action(benchmark.do);
 
 
 program.allowUnknownOption(false);
 
+
 const parsed = program.parse(process.argv);
 if (!(parsed.args && parsed.args.length > 0 && (typeof (parsed.args[0] === 'object')))) {
+  console.log(parsed.args.length);
   program.outputHelp();
 }
