94eeb4c76bd457f2d4d218c2633d9bf80de81860
Federico Gimenez <federico.gimenez@gmail.com>
2019-06-06 19:12:52 +0200

    cluster partitions

---
+++ test/lib/network/nodes.js
@@ -0,0 +1,59 @@
+const subject = require('../../../lib/network/nodes');
+
+
+let config;
+
+describe('nodes', () => {
+  beforeEach(() => {
+    config = {
+      nodes: 10,
+      remote: {
+        clusters: [
+          {location: 'location1'},
+          {location: 'location2'}
+        ]
+      }
+    };
+  });
+
+  describe('partition', () => {
+    it('should and array with as many elements as clusters', () => {
+      const clusterFixtures = [
+        [],
+        [
+          {location: 'location1'},
+          {location: 'location2'}
+        ],
+        [
+          {location: 'location1'},
+          {location: 'location2'},
+          {location: 'location3'},
+          {location: 'location4'},
+          {location: 'location5'},
+          {location: 'location6'}
+        ],
+        [
+          {location: 'location1'}
+        ]
+      ];
+
+      clusterFixtures.forEach((fixture) => {
+        config.remote.clusters = fixture;
+
+        const expected = fixture.length;
+        const actual = subject.partition(config).length;
+
+        actual.should.eq(expected);
+      })
+    });
+
+    it('should return the number of nodes per cluster with no remainder', () => {
+      const expected = [5, 5];
+      const actual = subject.partition(config);
+
+      actual.length.should.eq(expected.length);
+      actual[0].should.eq(expected[0]);
+      actual[1].should.eq(expected[1]);
+    });
+  });
+});
