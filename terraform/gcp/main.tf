cae5a2a424b00a24908810e50c4d641005ed6edb
Federico Gimenez <federico.gimenez@gmail.com>
2019-06-11 09:53:06 +0200

    use preemtible machines

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -26,7 +26,7 @@ resource "google_container_cluster" "primary" {
   }
 
   node_config {
-    preemptible  = false
+    preemptible  = true
     machine_type = var.machine_type
 
     oauth_scopes = [
