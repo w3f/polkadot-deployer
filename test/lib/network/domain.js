8eca6bb1de3ac3f9e574b344635ccccba55ffad3
Federico Gimenez <federico.gimenez@gmail.com>
2019-06-25 18:01:10 +0200

    added domain.telemetry

--- test/lib/network/domain.js
+++ test/lib/network/domain.js
@@ -33,4 +33,18 @@ describe('domain', () => {
       actual.should.eq(expected);
     });
   });
+  describe('telemetry', () => {
+    it('should return the telemetry domain for the given config and cluster index', () => {
+      const expected = 'wss://telemetry-backend.subdomain.domain.tld';
+      const actual = subject.telemetry(input, 1);
+
+      actual.should.eq(expected);
+    });
+    it('should return the default domain for the given config and 0 index if not specified', () => {
+      const expected = 'wss://telemetry-backend.subdomain.another-domain.com';
+      const actual = subject.telemetry(input);
+
+      actual.should.eq(expected);
+    });
+  });
 });
