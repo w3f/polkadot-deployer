ea35a7a41584abbc929773fb998ab0ba03bd49e3
Federico Gimenez <federico.gimenez@gmail.com>
2019-06-26 10:53:31 +0200

    domain.telemetry takes explicit arguments

--- test/lib/network/domain.js
+++ test/lib/network/domain.js
@@ -35,14 +35,8 @@ describe('domain', () => {
   });
   describe('telemetry', () => {
     it('should return the telemetry domain for the given config and cluster index', () => {
-      const expected = 'wss://telemetry-backend.subdomain.domain.tld';
-      const actual = subject.telemetry(input, 1);
-
-      actual.should.eq(expected);
-    });
-    it('should return the default domain for the given config and 0 index if not specified', () => {
-      const expected = 'wss://telemetry-backend.subdomain.another-domain.com';
-      const actual = subject.telemetry(input);
+      const expected = 'wss://telemetry-backend.subdomain-0.domain.tld';
+      const actual = subject.telemetry('subdomain', 'domain.tld');
 
       actual.should.eq(expected);
     });
