641ece1fe797f3ddbfdf90eded449d296ac5176a
Federico Gimenez <federico.gimenez@gmail.com>
2019-07-03 21:14:51 +0200

    open firewall for p2p

--- terraform/do/main.tf
+++ terraform/do/main.tf
@@ -1,3 +1,7 @@
+resource "digitalocean_tag" "expose-p2p" {
+  name = "expose-p2p"
+}
+
 resource "digitalocean_kubernetes_cluster" "primary" {
   name    = var.cluster_name
   region  = var.location
@@ -8,5 +12,17 @@ resource "digitalocean_kubernetes_cluster" "primary" {
     name       = "node-pool"
     size       = var.machine_type
     node_count = var.node_count
+    tags = ["${digitalocean_tag.expose-p2p.id}"]
+  }
+}
+
+resource "digitalocean_firewall" "fw-p2p" {
+  name = "fw-p2p"
+  tags = ["${digitalocean_tag.expose-p2p.id}"]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "30100-30200"
+    source_addresses = ["0.0.0.0/0", "::/0"]
   }
 }
