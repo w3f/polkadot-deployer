020030bf339ded12dc5946e7d2ae38f337ee4e5f
Federico Gimenez <federico.gimenez@gmail.com>
2019-07-04 10:58:56 +0200

    allow upper level domain for remote config

--- test/lib/network/domain.js
+++ test/lib/network/domain.js
@@ -5,9 +5,10 @@ require('chai')
 
 describe('domain', () => {
   describe('default', () => {
-    it('should return the default domain for the given config and cluster index', () => {
+    it('should return the default domain for the given name and config', () => {
       const expected = 'subdomain.domain.tld';
-      const actual = subject.default('subdomain', 'domain.tld');
+      const config = {clusters: [{domain: 'domain.tld'}]}
+      const actual = subject.default('subdomain', config);
 
       actual.should.eq(expected);
     });
@@ -19,5 +20,36 @@ describe('domain', () => {
 
       actual.should.eq(expected);
     });
+  });
+  describe('base', () => {
+    it('should return the domain defined at the cluster level specified by index', () => {
+      const expected = 'domain.tld';
+      const config = {clusters: [{domain: 'other-domain.tld'}, {domain: 'domain.tld'}]}
+      const actual = subject.base(config, 1);
+
+      actual.should.eq(expected);
+    });
+    it('should use cluster index 0 by default', () => {
+      const expected = 'domain0.tld';
+      const config = {clusters: [{domain: 'domain0.tld'}, {domain: 'domain1.tld'}]}
+      const actual = subject.base(config);
+
+      actual.should.eq(expected);
+    });
+    it('should use upper level domain if not defined at cluster level', () => {
+      const expected = 'domain.tld';
+      const config = {domain: 'domain.tld', clusters: [{field: 'value'}]}
+      const actual = subject.base(config);
+
+      actual.should.eq(expected);
+    });
+    it('should prefer domain defined at cluster level over upper level domain', () => {
+      const expected = 'cluster-domain.tld';
+      const config = {domain: 'upper-domain.tld', clusters: [{domain: 'cluster-domain.tld'}]}
+      const actual = subject.base(config);
+
+      actual.should.eq(expected);
+    });
+
   });
 });
