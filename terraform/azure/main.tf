bd7cd6c26cfde0195212a7dd96bbef2ca0021d52
Federico Gimenez <federico.gimenez@gmail.com>
2019-07-04 18:53:53 +0200

    fix resource group reference

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "polkadot" {
 
 resource "azurerm_kubernetes_cluster" "polkadot" {
   name                = var.cluster_name
-  location            = var.location
-  resource_group_name = "polkadot"
+  location            = "${azurerm_resource_group.polkadot.location}"
+  resource_group_name = "${azurerm_resource_group.polkadot.name}"
   dns_prefix          = "polkadot"
   kubernetes_version  = var.k8s_version
 
