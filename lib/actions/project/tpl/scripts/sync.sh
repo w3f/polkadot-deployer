6949c852340298e3e3387d56ab79a2294fc524fd
Federico Gimenez <federico.gimenez@gmail.com>
2019-07-09 17:52:39 +0200

    allow to pass params to polkadot-deployer from sync script

--- lib/actions/project/tpl/scripts/sync.sh
+++ lib/actions/project/tpl/scripts/sync.sh
@@ -4,5 +4,5 @@ basedir="$(dirname $0)"
 
 (
     cd $basedir/..
-    npx polkadot-deployer create --update -d '.' -c config.json
+    npx polkadot-deployer create --update -d '.' -c config.json "$@"
 )
