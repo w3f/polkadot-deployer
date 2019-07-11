09d31206d32f4cd2c7fe9d0f19d93a54722976da
Federico Gimenez <federico.gimenez@gmail.com>
2019-07-11 16:54:25 +0200

    specify k8s version from vars

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -121,6 +121,7 @@ resource "aws_security_group_rule" "polkadot-ingress-node-https" {
 resource "aws_eks_cluster" "polkadot" {
   name     = var.cluster_name
   role_arn = "${aws_iam_role.polkadot.arn}"
+  version = var.k8s_version
 
   vpc_config {
     security_group_ids = ["${aws_security_group.polkadot.id}"]
