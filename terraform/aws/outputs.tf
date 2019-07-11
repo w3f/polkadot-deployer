2177bb1dc9a6f8fded6cc9b2d1648f057b960ebd
Federico Gimenez <federico.gimenez@gmail.com>
2019-07-11 14:18:44 +0200

    fix kubeconfig output and add config_map_aws_auth output

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -1,4 +1,54 @@
+locals {
+  config_map_aws_auth = <<CONFIGMAPAWSAUTH
+
+
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${aws_iam_role.polkadot-node.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+CONFIGMAPAWSAUTH
+
+output "config_map_aws_auth" {
+  value = "${local.config_map_aws_auth}"
+}
+
+  kubeconfig = <<KUBECONFIG
+apiVersion: v1
+clusters:
+- cluster:
+    server: ${aws_eks_cluster.polkadot.endpoint}
+    certificate-authority-data: ${aws_eks_cluster.polkadot.certificate_authority.0.data}
+  name: kubernetes
+contexts:
+- context:
+    cluster: kubernetes
+    user: aws
+  name: aws
+current-context: aws
+kind: Config
+preferences: {}
+users:
+- name: aws
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1alpha1
+      command: aws-iam-authenticator
+      args:
+        - "token"
+        - "-i"
+        - "${var.cluster_name}"
+KUBECONFIG
+}
+
 output "kubeconfig" {
-  value = azurerm_kubernetes_cluster.polkadot-{{ clusterName }}.kube_config_raw
+  value = "${local.kubeconfig}"
   sensitive = true
 }
